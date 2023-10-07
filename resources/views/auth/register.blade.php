9af72d46e08a66c3b188058839dd037e9b4b80dc
antoniuskp <antoniuskustionop@gmail.com>
2023-10-07 23:01:20 +0700

    Update UI page Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,40 +20,54 @@
                     </div>
                     <form method="POST" action="{{ route('register') }}" class="form-login-register">
                         @csrf
-                        <input type="text" name="nama" placeholder="Nama Lengkap" class="@error('nama') is-invalid @enderror"
-                            required autofocus autocomplete="name">
-                        @error('nama')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                        <input type="text" id="nrp" name="nrp" placeholder="NRP" class="@error('nrp') is-invalid @enderror" maxlength="9" required>
-                        @error('nrp')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                        <!-- <input type="email" id="email" name="email" placeholder="Email Gooaya" class="@error('email') is-invalid @enderror"
-                            required readonly>
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror -->
-                        <input type="password" name="password" placeholder="Password"
-                            class="@error('password') is-invalid @enderror" required autocomplete="new-password">
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                        <input type="password" name="password_confirmation" placeholder="Retype Password"
-                            class="@error('password_confirmation') is-invalid @enderror" autocomplete="new-password">
-                        @error('password_confirmation')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
+                        <div class="transBox">
+                            <div class="transBox2">
+                                <p class="colorText">NAMA LENGKAP: </p>
+                                <input type="text" name="nama" placeholder="" class="@error('nama') is-invalid @enderror"
+                                required autofocus autocomplete="name">
+                            @error('nama')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            </div>
+                            <div class="transBox2">
+                                <p class="colorText">NRP: </p>
+                                <input type="text" id="nrp" name="nrp" placeholder="" class="@error('nrp') is-invalid @enderror" maxlength="9" required>
+                                @error('nrp')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                                <!-- <input type="email" id="email" name="email" placeholder="Email Gooaya" class="@error('email') is-invalid @enderror"
+                                    required readonly>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror -->
+                            </div>
+                            <div class="transBox2">
+                                <p class="colorText">PASSWORD: </p>
+                                <input type="password" name="password" placeholder=""
+                                class="@error('password') is-invalid @enderror" required autocomplete="new-password">
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            </div>
+                            <div class="transBox2">
+                                <p class="colorText">RETYPE PASSWORD: </p>
+                                <input type="password" name="password_confirmation" placeholder=""
+                                class="@error('password_confirmation') is-invalid @enderror" autocomplete="new-password">
+                            @error('password_confirmation')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            </div>
+                        </div>
                         <button class="btnLogin">Sign Up</button>
                         <p class="register">Already have an account? <a href="{{ url('/login') }}"> Login</a></p>
                     </form>
