58bfe5d97aaf91c42922526416c1d5715d3885d4
antoniuskp <antoniuskustionop@gmail.com>
2023-10-07 22:17:24 +0700

    Update UI page login
    change logo, transparent box, and other

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.mainweb')
 
 @section('title')
-    PIMUS 12 - Login
+    PIMUS 13 - Login
 @endsection
 
 @section('content')
@@ -13,23 +13,27 @@
                     <img src="{{ url('/assets/images/logo/logo-pimus-text.png') }}" alt="PIMUS XI" class="mb-4">
                     <form method="POST" action="{{ route('login') }}" class="form-login-register">
                         @csrf
-                        <div>
-                            <input id="email" type="text" class="@error('email') is-invalid @enderror" placeholder="sNRP"
-                                name="email" required autocomplete="text" maxlength=10 autofocus>
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                        <div>
-                            <input id="password" type="password" class="@error('password') is-invalid @enderror"
-                                placeholder="Password" name="password" required autocomplete="current-password">
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                        <div class="transBox">
+                            <div class="transBox2">
+                                <p class="colorText">USERNAME: </p>
+                                <input id="email" type="text" class="@error('email') is-invalid @enderror" placeholder=""
+                                    name="email" required autocomplete="text" maxlength=10 autofocus>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="transBox2">
+                                <p class="colorText">PASSWORD: </p>
+                                <input id="password" type="password" class="@error('password') is-invalid @enderror"
+                                    placeholder="" name="password" required autocomplete="current-password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
                         @if (Route::has('password.request'))
                             <a class="forget" href="{{ route('password.request') }}">
